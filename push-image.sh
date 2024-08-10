88acfbe3a5ca01d5d71f0f0b112009ce8305ef99
prastdon <dprasetiyo6@gmail.com>
2024-08-10 13:56:24 +0700

    finalize dockerfile for web

--- push-image.sh
+++ push-image.sh
@@ -1,6 +1,6 @@
 aws ecr get-login-password --region $3 | docker login --username AWS --password-stdin $2
 
-docker build --platform linux/amd64 -t $1 -f apps/$1/Dockerfile . --build-arg APP_NAME=$1 --build-arg PORT=$4
+docker build --platform linux/amd64 -t $1 -f tooling/docker/$5/Dockerfile . --build-arg APP_NAME=$1 --build-arg PORT=$4
 
 docker tag $1:latest $2:latest
 
