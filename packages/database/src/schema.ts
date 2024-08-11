1d2a7f23402802157d0139787135241f43833821
prastdon <dprasetiyo6@gmail.com>
2024-08-11 19:06:15 +0700

    fix missing type

--- packages/database/src/schema.ts
+++
@@ -1,4 +0,0 @@
-import { z } from "zod";
-import { SubscriptionType } from "@repo/shared-types";
-
-export const subscriptionTypeSchema = z.nativeEnum(SubscriptionType);
--- packages/shared-types/src/index.ts
+++ packages/shared-types/src/index.ts
@@ -1,3 +1,4 @@
 export * from './database/types'
 export * from './database/enums'
+export * from './database/utils'
 export * from './utils/PickStartsWith'
