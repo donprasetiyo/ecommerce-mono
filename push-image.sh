906a52efa443d4aea5932c311b1a989c957e705b
prastdon <dprasetiyo6@gmail.com>
2024-08-13 17:31:11 +0700

    fix missing kafka secret folder

--- push-image.sh
+++ push-image.sh
@@ -1,6 +1,6 @@
 aws ecr get-login-password --region $3 | docker login --username AWS --password-stdin $2
 
-docker build --platform linux/amd64 -t $1 -f tooling/docker/$5/Dockerfile . --build-arg APP_NAME=$1 --build-arg PORT=$4
+docker build --platform linux/amd64 -t $1 -f tooling/docker/$5/Dockerfile . --build-arg APP_NAME=$1 --build-arg PORT=$4 --build-arg KAFKA_SSL_SECRETS_DIR=$6
 
 docker tag $1:latest $2:latest
 
